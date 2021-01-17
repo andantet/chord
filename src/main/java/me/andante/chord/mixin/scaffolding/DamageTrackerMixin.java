c5c0eb0efd3bc0cd71225bb38f66fbd8218efbd4
Daniel Orr <orrdaniel73@gmail.com>
2021-01-17 15:22:44 +0000

    fix scaffolding bugs
    + custom death suffixes for custom scaffolding
    * fixed shifted downwards movement on custom scaffolding

---
+++ src/main/java/me/andante/chord/mixin/scaffolding/DamageTrackerMixin.java
@@ -0,0 +1,34 @@
+package me.andante.chord.mixin.scaffolding;
+
+import me.andante.chord.block.CScaffoldingBlock;
+import net.minecraft.block.Block;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageTracker;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+@Mixin(DamageTracker.class)
+public class DamageTrackerMixin {
+    @Shadow @Final
+    private LivingEntity entity;
+    @Shadow @Mutable
+    private String fallDeathSuffix;
+
+    @Inject(method = "setFallDeathSuffix", at = @At("TAIL"))
+    private void setFallDeathSuffix(CallbackInfo ci) {
+        Optional<BlockPos> optional = this.entity.getClimbingPos();
+        if (optional.isPresent()) {
+            Block block = this.entity.world.getBlockState(optional.get()).getBlock();
+            if (block instanceof CScaffoldingBlock) this.fallDeathSuffix = Registry.BLOCK.getId(block).toString();
+        }
+    }
+}
---
+++ src/main/java/me/andante/chord/mixin/scaffolding/LivingEntityMixin.java
@@ -0,0 +1,22 @@
+package me.andante.chord.mixin.scaffolding;
+
+import me.andante.chord.block.CScaffoldingBlock;
+import net.minecraft.block.Block;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(method = "applyClimbingSpeed", at = @At("RETURN"), cancellable = true)
+    private void applyClimbingSpeed(Vec3d motion, CallbackInfoReturnable<Vec3d> cir) {
+        LivingEntity $this = LivingEntity.class.cast(this);
+        Block block = $this.getBlockState().getBlock();
+        if ($this.isClimbing() && block instanceof CScaffoldingBlock && $this.isHoldingOntoLadder()) {
+            cir.setReturnValue(cir.getReturnValue().add(0.0D, -0.15000000596046448D, 0.0D));
+        }
+    }
+}
